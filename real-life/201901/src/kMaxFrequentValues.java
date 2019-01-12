bf7f934d9d26ababd488bef33e1cff6e73a72b18
Igor F. Yago <igor.yago@gmail.com>
2019-01-12 16:01:47 +0000

    functional_java8_streams

--- real-life/201901/src/kMaxFrequentValues.java
+++ real-life/201901/src/kMaxFrequentValues.java
@@ -1,18 +1,18 @@
 import java.util.*;
 import static java.util.stream.Collectors.*;
 
-public class topKbyGroup {
-    // TODO: Return the 'k' most frequent elements in a list of Integers.
+public class kMaxFrequentValues {
+    // TODO: Return the 'k' most frequent values in a list of Integers.
     
     public static void main(String[] args) {
-        List<Integer> elements = Arrays.asList(3, 5, 1, 1, 1, 2, 1, 4, 4, 4, 0, 5);
+        List<Integer> values = Arrays.asList(3, 5, 1, 1, 1, 2, 1, 4, 4, 4, 0, 5);
         int K = 3;
 
-        System.out.println( topKbyGroup(elements, K) );
+        System.out.println(kMaxFrequentValues(values, K));
     }
 
-    public static List<Integer> topKbyGroup(List<Integer> elements, int K) {
-        Map<Integer, Long> groupMap = elements.stream()
+    public static List<Integer> kMaxFrequentValues(List<Integer> values, int K) {
+        Map<Integer, Long> groupMap = values.stream()
                 .parallel()
                 //.peek(System.out::println)
                 .collect(groupingBy(e -> e, counting()));
--- real-life/201901/src/maxUniqueSubListValues.java
+++ real-life/201901/src/maxUniqueSubListValues.java
@@ -2,21 +2,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class uniqueSubListNM {
-    // TODO: Return the max number of distinct elements among the rolling consecutive sub-lists of size M, for a given list of N integers
+public class maxUniqueSubListValues {
+    // TODO: Return the max number of unique values among the rolling consecutive sub-lists of size M, for a given list of N integers
 
     public static void main(String[] args) {
-        List<Integer> elements = Arrays.asList(1, 1, 3, 3, 5, 3);
-        int N = elements.size();
+        List<Integer> values = Arrays.asList(1, 1, 3, 3, 5, 3);
+        int N = values.size();
         int M = 3;
 
-        System.out.println( maxUniqueCount(elements, N, M) );
+        System.out.println(maxUniqueSubList(values, N, M));
     }
 
-    public static Long maxUniqueCount(List<Integer> elements, int N, int M) {
+    public static Long maxUniqueSubList(List<Integer> values, int N, int M) {
         return IntStream.range(0, N - M + 1)
                 .parallel()
-                .mapToObj(i -> elements.subList(i, i + M))
+                .mapToObj(i -> values.subList(i, i + M))
                 //.peek(System.out::println)
                 .map(e -> e.stream()
                         .parallel()
